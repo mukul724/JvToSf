1d6a97cbf8ae7f9aa55de4566cc11b36e86e9f20
Mukul Kumar <mukul1613kumar@outlook.com>
2015-02-05 15:58:25 +0530

    Signed-off-by: Mukul Kumar <mukul1613kumar@outlook.com>

--- src/CSVToString.java
+++ src/CSVToString.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class CSVToString {
    	
-	static String SF_Data = "primaryEmail,contactEmail,firstName,middleName,lastName,cellPhone,homePhone,address,address2,city,state,zip,country,dateOfBirth,gender,ssn,disabilityStatus,preferredLoc,contactOtherPosition,comments,resume";
-    static String JV_Mapping="Email,Email,FirstName,middleName,LastName,Mobile,HomePhone,Address,address2,City,State,PostalCode,Country,dateOfBirth,Gender,ssn,disabilityStatus,Location,contactOtherPosition,comments,OriginalDocument";
+	static String SF_Data = "primaryEmail,contactEmail,firstName,middleName,lastName,cellPhone,homePhone,address,address2,city,state,zip,country,dateOfBirth,gender,ssn,disabilityStatus,preferredLoc,contactOtherPosition,comments,commentator,resume";
+    static String JV_Mapping="Email,Email,FirstName,middleName,LastName,Mobile,HomePhone,Address,address2,City,State,PostalCode,Country,dateOfBirth,Gender,ssn,disabilityStatus,Location,contactOtherPosition,comments,commentator,OriginalDocument";
 	static String SF_Columns[] = SF_Data.split(",");
     static String JV_Columns[] = JV_Mapping.split(",");
  
--- src/XMLDirectParse.java
+++ src/XMLDirectParse.java
@@ -27,28 +27,53 @@ public class XMLDirectParse {
 		return "";
 	}
 
+	
+	public static String filterPhoneNumber(String data){
+
+		switch (data.length()) {
+
+		case 10:
+			return data;
+			
+		case 11:
+			data=(data.charAt(0)=='0')?data.substring(1):"9999999999";
+			 
+			return data;
+		
+		case 12:
+		
+			data=(data.substring(0,1)=="91")?data.substring(2):"9999999999";
+				 
+			return data;
+		
+		case 13:
+			data=(data.substring(0,2)=="+91")?data.substring(3):"9999999999";
+			
+			return data;
+
+		default:
+			return "9999999999";
+
+		}
+			
+	}
+	
+	
 	public static void main(String[] args) throws ParserConfigurationException,
 			SAXException, IOException {
 		DocumentBuilderFactory DBF = DocumentBuilderFactory.newInstance();
 		DocumentBuilder Doc = DBF.newDocumentBuilder();
 
-		String inputFileName = "C:/USERS/MUKUL.KUMAR/PICTURES/UPLOADS/UNZIPPED/FLIPKARTINDIAPVTLTD_10012014-12312014_12162014233658/CANDIDATES/CANDIDATERECORDSA_";
-		String outputFileName = "C://work//Candidate_Data_Output//CandidateRecordsA_";
+		//String inputFileName = "C:/USERS/MUKUL.KUMAR/PICTURES/UPLOADS/UNZIPPED/FLIPKARTINDIAPVTLTD_10012014-12312014_12162014233658/CANDIDATES/CANDIDATERECORDSA_";
+		//String outputFileName = "C://work//Candidate_Data_Output//CandidateRecordsA_";
+		String inputFileName ="C:/Users/mukul.kumar/Desktop/sample data/CandidateRecordsA_";
+		String outputFileName ="C:/Users/mukul.kumar/Desktop/sample data/OutputCandidateRecordsA_";
 		int candidateCount = 0;
 		int inputFileIndex = 0, outputFileIndex = 0;
-		int NO_OF_CANDIDATE_FILES = 10;
-        int MAX_CANDIDATES_PER_SHEET=1000;
-        CSVWriter writer=null;
+		int NO_OF_CANDIDATE_FILES = 1;
+		int MAX_CANDIDATES_PER_SHEET = 50000;
+		CSVWriter writer = null;
 		for (; inputFileIndex < NO_OF_CANDIDATE_FILES; inputFileIndex++) {
-			
-			//String csv = outputFileName + outputFileIndex + ".csv"; // Output
-																	// CSV file
-
-			//CSVWriter writer = new CSVWriter(new FileWriter(csv)); // writer object writer.write(string)
-
-			// CSVWriter writer = new CSVWriter(new FileWriter(csv),
-			// CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER);
-
 			String[] JV_Columns = CSVToString.getJVColumns();
 
 			File CandidateFile = new File(inputFileName + inputFileIndex
@@ -60,11 +85,6 @@ public class XMLDirectParse {
 
 				NodeList candidateList = docEle
 						.getElementsByTagName("Candidate");
-				//writer.writeNext(CSVToString.getSFColumns()); // for writing the
-																// column names
-																// in
-																// the first row
-
 				String[] tempCandidateRow = new String[JV_Columns.length];
 				// NodeList[] tempList = new NodeList[JV_Columns.length];
 				ArrayList<NodeList> tempList = new ArrayList<NodeList>();
@@ -93,22 +113,88 @@ public class XMLDirectParse {
 							tempCandidateRow[j] = "";
 					}
 
-					
-					if(candidateCount++%MAX_CANDIDATES_PER_SHEET==0){  // creating new files on the basis of max candidates per file
-						if(writer!=null)
+					if (candidateCount++ % MAX_CANDIDATES_PER_SHEET == 0) { // creating
+																			// new
+																			// files
+																			// on
+																			// the
+																			// basis
+																			// of
+																			// max
+																			// candidates
+																			// per
+																			// file
+						if (writer != null)
 							writer.close();
-						String csv = outputFileName + (outputFileIndex++) + ".csv";
-						writer = new CSVWriter(new FileWriter(csv)); // writer object writer.write(string)
+						String csv = outputFileName + (outputFileIndex++)
+								+ ".csv";
+						writer = new CSVWriter(new FileWriter(csv)); // writer
+																		// object
+																		// writer.write(string)
 						writer.writeNext(CSVToString.getSFColumns());
 					}
+                 if(tempCandidateRow[0]!=""&&tempCandidateRow[0]!=null){
 					
-					
-					writer.writeNext(tempCandidateRow);
-				}
+                	 //2-fname, 4-lname, 5-cellphone,6-homephone, 9-city,13-dob, 15-ssn, 16-disability status,
+                	 // 17-preferred location, 18-contact other position
+                	 //19- comment,20 - commentator
+                	 
+                	 if(tempCandidateRow[2]==""||tempCandidateRow[2]==null) {
+                		 tempCandidateRow[2]="NA";
+                	 }
+                	 
+                	 if(tempCandidateRow[4]==""||tempCandidateRow[4]==null) {
+                		 tempCandidateRow[4]="NA";
+                	 }
+                	 if(tempCandidateRow[5]==""||tempCandidateRow[5]==null) {
+                		 tempCandidateRow[5]=(tempCandidateRow[6]==""||tempCandidateRow[6]==null)?"9999999999":tempCandidateRow[6];
+                		 //"NA"; //9999999999
+                	 }
+                	 if(tempCandidateRow[6]==""||tempCandidateRow[6]==null) {
+                		 tempCandidateRow[6]=tempCandidateRow[5];  
+                	 }
+                	 
+                	 tempCandidateRow[5]= tempCandidateRow[5].replaceAll("[^0-9]", "");
+                	 tempCandidateRow[6]= tempCandidateRow[6].replaceAll("[^0-9]", "");
+                	 tempCandidateRow[5]= filterPhoneNumber(tempCandidateRow[5]); 
+                	 tempCandidateRow[6]= filterPhoneNumber(tempCandidateRow[6]);
+ 
+                	 if(tempCandidateRow[9]==""||tempCandidateRow[9]==null) { 
+                		 tempCandidateRow[9]="NA";
+                	 }
+                	 
 
-				//writer.close(); // close the writer
+                	// if(tempCandidateRow[12]==""||tempCandidateRow[12]==null) {
+                		 tempCandidateRow[12]="IN";
+                	
+                	 
+                	 if(tempCandidateRow[13]==""||tempCandidateRow[13]==null) {
+                		 tempCandidateRow[13]="2015-02-02T00:00:00";
+                	 }
+                	 if(tempCandidateRow[15]==""||tempCandidateRow[15]==null) {
+                		 tempCandidateRow[15]="NA";
+                	 }
+                	 if(tempCandidateRow[16]==""||tempCandidateRow[16]==null) {
+                		 tempCandidateRow[16]="No";
+                	 }
+                	 if(tempCandidateRow[17]==""||tempCandidateRow[17]==null) {
+                		 tempCandidateRow[17]="Bengaluru";
+                	 }
+                	 if(tempCandidateRow[18]==""||tempCandidateRow[18]==null) {
+                		 tempCandidateRow[18]="Yes";
+                	 }
+                	 if(tempCandidateRow[19]==""||tempCandidateRow[19]==null) {
+                		 tempCandidateRow[19]="This is JobVite Data";
+                	 }
+                	 if(tempCandidateRow[20]==""||tempCandidateRow[20]==null) {
+                		 tempCandidateRow[20]="recruit.admin";
+                	 }
+                	 writer.writeNext(tempCandidateRow);
+					}
+				}
 			}
 		}
-		writer.close(); //close the writer
+		writer.close(); // close the writer
 	}
 }
+
